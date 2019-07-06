077b1194cfddb8635ec1f3f12a83553e3184c561
Amit Azulay <amitazulay@Amits-MacBook-Pro.local>
2019-07-06 12:09:39 +0300

    Finished My FrameWork

---
+++ OTP/OTP/OTP.h
@@ -0,0 +1,19 @@
+//
+//  OTP.h
+//  OTP
+//
+//  Created by Amit Azulay on 06/07/2019.
+//  Copyright © 2019 Amit Azulay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OTP.
+FOUNDATION_EXPORT double OTPVersionNumber;
+
+//! Project version string for OTP.
+FOUNDATION_EXPORT const unsigned char OTPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OTP/PublicHeader.h>
+
+
---
+++ OTP/OTP/Service.swift
@@ -0,0 +1,20 @@
+//
+//  Service.swift
+//  OTP
+//
+//  Created by Amit Azulay on 06/07/2019.
+//  Copyright © 2019 Amit Azulay. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomting() -> String {
+        
+        return "Did Some Stuff, here you go homeboi!"
+    }
+    
+}
